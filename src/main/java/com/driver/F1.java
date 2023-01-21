851fa769022563f32ce3e1817bcc52dcad72c187
vipin <vipinkmundra@gmail.com>
2023-01-21 18:04:41 +0530

    solution3

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -4,7 +4,7 @@ public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
-        super(name,4,1,6,isManual,"",1);
+        super(name,4,1,6,isManual,"vipin",1);
     }
 
     public void accelerate(int rate){
